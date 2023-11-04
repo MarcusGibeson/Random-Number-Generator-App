7d6d3466eee7195c299e7c31184ba954cf405ff8
Marcus Gibeson <133388744+MarcusGibeson@users.noreply.github.com>
2023-11-03 23:53:20 -0400

    commit

--- index.php
+++ index.php
@@ -1,48 +1,47 @@
+<?php
+session_start();
+    function rollD20() {
+        return mt_rand(1,20);
+    }
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['roll'])){
+        $rollResult = rollD20();
+        $imagePath = "images/d20_" . $rollResult . ".png";
+        $_SESSION['rollResult'] = $rollResult;
+        $_SESSION['imagePath'] = $imagePath;
+    }
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>20 Side Die</title>
 </head>
 <body>
-    <h1> Roll a 20-side dice </h1>
+    <h1> Roll a 20-side die </h1>
     Goal of Site: Create a 20 side die to roll randomly generated number.
 
     <?php
-        function rollD20() {
-            return mt_rand(1,20);
+        if (isset($_SESSION['rollResult'])){
+            echo "<p>You rolled a " . $_SESSION['rollResult']. " on the d20!</p>";
+            echo "<img src='" . $_SESSION['imagePath'] . "' alt='D20 result'>";
         }
-
-        if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['roll'])){
-            $rollResult = rollD20();
-            $imagePath = "images/d20_" . $rollResult . ".png";
-            echo "<p>You rolled a $rollResult on the d20!</p>";
-            echo "<img src='$imagePath' alt='D20 result'>";
-        }
-
     ?>
 
     <form method="post" id="rollForm">
         <input type="submit" name="roll" value="Roll the D20">
     </form>
+    
+    <?php
+        if (isset($_SESSION['rollResult'])) {
+            unset($_SESSION['rollResult']);
+            unset($_SESSION['imagePath']);
+        }
+    ?>
 
-    <script> 
-        var form = document.getElementById('rollForm');
-        var resultContainer = document.getElementById('resultContainer');
-        
-        document.getElementById('rollForm').addEventListener('submit', function (e) {
-            e.preventDefault();
-            var xhr = new XMLHttpRequest();
-            xhr.open('POST', form.action, true);
-            xhr.onload = function () {  
-                if (xhr.status === 200) {
-                    resultContainer.innerHTML = xhr.responseText;
-                }
-            };
-            xhr.send(new FormData(form));
-        });
-        </script>
-        <div id="resultContainer"></div>
 </body>
 </html>
